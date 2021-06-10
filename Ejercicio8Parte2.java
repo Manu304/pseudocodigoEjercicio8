165e3716b28f86db6eef8ecc9c496ac5ee1faf1b
Manuel Rojas <manuerjs70@gmail.com>
2021-06-09 23:00:13 -0600

    Agregue gitignore y arachivo java de ejercicio 8

---
+++ Ejercicio8Parte2.java
@@ -0,0 +1,9 @@
+//202030799 Manuel Rojas
+//Dise�ar un algoritmo que genere n�meros al azar
+//m�ltiplos de 5, mientras el usuario no ingresa la opci�n 'S' (Salir)
+public class Ejercicio8Parte2 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
