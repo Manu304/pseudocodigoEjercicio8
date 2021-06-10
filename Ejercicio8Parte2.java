66b63f581f8cf506b035ad36d16a834c77646f35
Manuel Rojas <manuerjs70@gmail.com>
2021-06-09 23:49:37 -0600

    Agregue algoritmo del ejercicio8

--- Ejercicio8Parte2.java
+++ Ejercicio8Parte2.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 //202030799 Manuel Rojas
 //Dise�ar un algoritmo que genere n�meros al azar
 //m�ltiplos de 5, mientras el usuario no ingresa la opci�n 'S' (Salir)
 public class Ejercicio8Parte2 {
     public static void main(String[] args) {
-        
+        char salir = 'N';
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Presione 'S' para dejar de generar numeros: ");
+        while (salir != 'S'){
+            salir = scanner.next().toUpperCase().charAt(0);
+            if(salir != 'S'){
+                int random = 0;
+                do {
+                    random = (int) Math.floor(Math.random()*(1000+1));
+                } while ((random%5) != 0);
+                System.out.println(random);
+            }
+        }
     }
-    
 }
